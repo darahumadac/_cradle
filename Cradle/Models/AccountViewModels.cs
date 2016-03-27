47bcf0bba543a5b1f069be2dbc9d0615ed0c8424
Darah U <darahfumadac@gmail.com>
2016-03-27 21:19:24 +0800

    Initial Register page layout only

--- Cradle/Models/AccountViewModels.cs
+++ Cradle/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cradle.Models
 {
@@ -46,11 +47,38 @@ namespace Cradle.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        [Required]
+        [Display(Name = "Mobile Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string MobileNo { get; set; }
+
+        [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        //Password: Use at least one lowercase letter, one numeral, and seven characters.
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 7)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -59,5 +87,17 @@ namespace Cradle.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Register As")]
+        public AccountType MemberAccountType { get; set; }
+      
+    }
+
+    public enum AccountType 
+    {
+        //Anonymous = 1, //Site visitor.  Can search, share links, can order, and use the site but cannot rate
+        Member = 1, //Site member.  Can search, share links, and use the site, can rate designers, can save favorite designers
+        Designer = 2 //Seller.  Can create designer profile, upload fashion collections, can receive orders via e-mail
     }
 }
