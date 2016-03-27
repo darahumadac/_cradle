33b1650ecab7012c7d97177207a24195b02a8d0f
Darah U <darahfumadac@gmail.com>
2016-03-27 17:28:52 +0800

    Initial commit

---
+++ Cradle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cradle.MvcApplication" Language="C#" %>
---
+++ Cradle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cradle.Startup))]
+namespace Cradle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
